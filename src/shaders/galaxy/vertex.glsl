b97f5d538f8c517874c97fb3494e2c0ab8ba1d7a
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-25 17:05:40 -0500

    Extract fragment and vertex shaders into individual files

---
+++ src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,11 @@
+void main() {
+  // Position
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectionPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectionPosition;
+
+  // Size
+  gl_PointSize = 1.0;
+}
