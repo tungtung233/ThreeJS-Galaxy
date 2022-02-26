4568c804fc7263a0cc0cdc646f526b150111aab2
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-25 23:11:08 -0500

    Add softness to particle edge

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,8 +1,18 @@
 void main() {
-  // Disc
+  // // Disc
+  // float strength = distance(gl_PointCoord, vec2(0.5));
+  // strength = step(0.5, strength);
+  // strength = 1.0 - strength;
+
+  // // Diffuse
+  // float strength = distance(gl_PointCoord, vec2(0.5));
+  // strength *= 2.0;
+  // strength = 1.0 - strength;
+
+  // Light point
   float strength = distance(gl_PointCoord, vec2(0.5));
-  strength = step(0.5, strength);
   strength = 1.0 - strength;
+  strength = pow(strength, 5.0);
 
   gl_FragColor = vec4(vec3(strength), 1.0);
 }
